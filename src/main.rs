e4d31ced5b4f1baab2f6e1e195a21bdc681a8666
Rustixir <danlamhai@gmail.com>
2023-07-21 14:50:33 +0330

    added init

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,31 @@ pub mod repo;
 pub mod handler;
 pub mod dto;
 
-use darkbird::{Options, Storage, StorageType};
-use movie::Movie;
+use actix_web::{HttpServer, App, web};
 use repo::Repository;
 
 
 
-#[tokio::main]
-async fn main() {
-
-    let repo = Repository::new().await;
-
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     
+    let repo =  Repository::new().await;
+    let counter = web::Data::new(report);
+
+    HttpServer::new(move || {
+        // move counter into the closure
+        App::new()
+            .app_data(counter.clone()) 
+            .service(
+                web::scope("/movies")
+                    .service(handler::add)
+                    .service(handler::get)
+                    .service(handler::get_by_name)
+                    .service(handler::gets_by_tag)
+                    .service(handler::search)
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
